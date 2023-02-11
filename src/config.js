5d965fd7fa507fdb9d3aca6dda0457fa5119708a
Paul Matthew <68151903+mattemattical@users.noreply.github.com>
2023-02-11 08:27:51 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "token": "MTA3MzY4MjI5MzU3MjUwMTYxNQ.G5qpnD.uTgZHmu5w9F2pJrGFLn-GS3EfnqsdvU2Mz-oQg",
+  "token": "MTA3MzY4MjI5MzU3MjUwMTYxNQ.GU-Qdw.EK51dklk61iwBRUK3xKQreS7zAP62HuaPoQWd8",
   "language": true
 }
