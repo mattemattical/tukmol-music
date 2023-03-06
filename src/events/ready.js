cd2baf980defd0c76d17f83d5bd217b1d26eb83f
Paul Matthew <68151903+mattemattical@users.noreply.github.com>
2023-03-07 05:26:18 +0800

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,30 @@
-const { ActivityType } = require("discord.js")
+const { ActivityType } = require("discord.js");
+
 module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-  client.user.setActivity("🎵 | Tukmol Music")
-}};
+    name: "ready",
+    once: true,
+    async execute(client) {
+        console.log(`${client.user.tag} is ready!`);
+
+        let activities = [
+                `music with EPI#1372`,
+                `${client.user.username}`,
+                `${
+                    client.guilds.cache.size
+                } servers | ${client.guilds.cache.reduce(
+                    (a, b) => a + b.memberCount,
+                    0
+                )} users`,
+                `🐛${client.commands.size} commands!`,
+            ],
+            i = 0;
+        setInterval(
+            () =>
+                client.user.setActivity({
+                    name: `${activities[i++ % activities.length]}`,
+                    type: ActivityType.Listening,
+                }),
+            10000
+        );
+    },
+};
